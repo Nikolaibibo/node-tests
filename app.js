6c1e98564f596878ed30db8038ffa3e139ee45d5
Nikolaibibo <nbockholt@fork.de>
2015-01-26 13:57:17 +0100

    updated the text

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world!');
+    res.end('Hallo schöne neue Welt!');
 });
 server.listen(3000);
