71086376b633db8244ef56b7e0ec98c8910324be
Nikolaibibo <nbockholt@fork.de>
2015-01-26 14:12:46 +0100

    added express and stuff
    to test

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+/*
+
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hallo schöne neue Welt!');
 });
 server.listen(3000);
+
+*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hallo Welt du geile Sau!</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
