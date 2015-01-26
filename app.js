cce436948dade98e13a173cd4fa672e16c0fe304
Nikolaibibo <nbockholt@fork.de>
2015-01-26 14:16:06 +0100

    asd

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hallo Welt du geile Sau!</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
